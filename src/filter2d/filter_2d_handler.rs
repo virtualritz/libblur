fa4aee3db001e0a61c16eafbb44ab9f5cef2f208
Moritz Moeller <virtualritz@protonmail.com>
2025-07-25 10:49:43 +0200

    Added nightly_f16 feature.

--- src/filter2d/filter_2d_handler.rs
+++ src/filter2d/filter_2d_handler.rs
@@ -186,7 +186,11 @@ default_2d_column_handler!(u32, f32, true);
 default_2d_column_handler!(u32, f64, true);
 default_2d_column_handler!(u64, f32, true);
 default_2d_column_handler!(u64, f64, true);
-default_2d_column_handler!(f32, f32, false);
-default_2d_column_handler!(f32, f64, false);
-default_2d_column_handler!(f64, f32, false);
-default_2d_column_handler!(f64, f64, false);
+#[cfg(feature = "nightly_f16")]
+default_2d_column_handler!(f16, f32, true);
+#[cfg(feature = "nightly_f16")]
+default_2d_column_handler!(f16, f64, true);
+default_2d_column_handler!(f32, f32, true);
+default_2d_column_handler!(f32, f64, true);
+default_2d_column_handler!(f64, f32, true);
+default_2d_column_handler!(f64, f64, true);
